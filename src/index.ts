d4cfdfb983451b2f4a1f5c8f4f1805d1b27b4cd7
mmr011 <mmr001@gmail.com>
2026-01-26 10:32:59 -0400

    Configuration
    Initial configurations of Express server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Application, Response } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import cors from 'cors';
 
 // configs
 dotenv.config();
 const PORT = process.env.PORT;
 const app: Application = express();
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get('/', (_, res: Response) => {
-  res.status(200).send('Hello Guys!');
+  res.status(200).send('Hello World!');
 });
 
 app.listen(PORT, () => {
